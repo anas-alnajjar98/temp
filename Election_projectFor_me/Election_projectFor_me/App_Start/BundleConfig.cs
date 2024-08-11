df8bb10be6888497501d0d271bb6db78cf0106dc
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-08-11 22:07:03 +0300

    for group

---
+++ Election_projectFor_me/Election_projectFor_me/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Election_projectFor_me
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // You can also include the CSS files in a similar way if needed
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/assets/css/animate.css",
+                "~/assets/css/swiper-bundle.min.css",
+                "~/assets/css/magnific-popup.css",
+                "~/assets/css/leaflet.css",
+                "~/assets/css/leaflet-routing-machine.css",
+                "~/assets/css/style.css"
+            ));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Election_projectFor_me/Election_projectFor_me/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Election_projectFor_me.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private static string EncryptionKey = System.Configuration.ConfigurationManager.AppSettings["EncryptionKey"];
+
+        private static string Decrypt(string cipherText)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+        public ActionResult Index(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                string decryptedId = Decrypt(id);
+                int userId = int.Parse(decryptedId);
+
+                // Your logic here
+
+                return View(userId);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
